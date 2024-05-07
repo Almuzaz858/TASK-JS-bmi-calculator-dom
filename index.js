468c27485c12162d0b356630ba6f592ebdddbe77
Ziad Almuzaini <zman20000000@gmail.com>
2024-05-07 19:43:55 +0300

    task done

--- index.js
+++ index.js
@@ -8,4 +8,17 @@ function calculateBMI() {
   console.table({ age, weight, height, male, female });
 
   // You are going to write the code here before the curly braces 👇🏻
+  height = height/100
+  let BMI = weight/(height*height)
+  window.alert(BMI) 
+  if (BMI <= 24.9) {
+    alert('You are underweight')
+  } else if (BMI >= 25 && BMI <= 29.9) {
+    alert('You are healthy')
+  } else{
+    alert('You are overweight')
+  }
 }
+
+
+  
